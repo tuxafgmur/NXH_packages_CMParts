b2dc2e7fd9995e29f66302f86cd0bf4e74375229
dadi11 <dadi.sikorski@gmail.com>
2017-01-13 20:44:17 +0100

    CMParts: remove contributors

--- src/org/cyanogenmod/cmparts/BootReceiver.java
+++ src/org/cyanogenmod/cmparts/BootReceiver.java
@@ -22,7 +22,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.preference.PreferenceManager;
 
-import org.cyanogenmod.cmparts.contributors.ContributorsCloudFragment;
 import org.cyanogenmod.cmparts.input.ButtonSettings;
 
 public class BootReceiver extends BroadcastReceiver {
@@ -37,9 +36,6 @@ public class BootReceiver extends BroadcastReceiver {
             ButtonSettings.restoreKeyDisabler(ctx);
             setRestoredTunable(ctx);
         }
-
-        // Extract the contributors database
-        ContributorsCloudFragment.extractContributorsCloudDatabase(ctx);
     }
 
     private boolean hasRestoredTunable(Context context) {
